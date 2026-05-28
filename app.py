2a56546b788e880b0faae756c92085b61dba4a15
ankitadebnath1408 <debnathankita299@gmail.com>
2026-05-28 13:48:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(
 
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # =========================
 # LOAD MODEL
